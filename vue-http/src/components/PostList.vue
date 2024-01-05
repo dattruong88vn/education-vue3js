897c4cdc390cc61f1ea1df1e1bc022d9631a1df9
dat.truong <dat.truong@secomm.vn>
2024-01-05 13:32:33 +0700

    vue and http: get request

---
+++ vue-http/src/components/PostList.vue
@@ -0,0 +1,38 @@
+<template>
+  <button @click="getPosts">Load posts</button>
+  <h3 v-if="errorMessage">{{ errorMessage }}</h3>
+  <div v-for="post in posts" :key="post.id">
+    <h3>{{ post.id }}. {{ post.title }}</h3>
+    <p>{{ post.body }}</p>
+    <br />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "PostList",
+  data() {
+    return {
+      posts: [],
+      errorMessage: "",
+    };
+  },
+  methods: {
+    async getPosts() {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        this.posts = response.data;
+      } catch (error) {
+        console.log(error);
+        this.errorMessage = "Something went wrong";
+      }
+    },
+  },
+};
+</script>
+
+<style scoped></style>
